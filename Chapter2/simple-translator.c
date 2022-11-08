68c940e896c907589fce0dccda1d70f67ad916e2
shannaurelle <shannaurelleg@gmail.com>
2022-11-08 22:13:11 +0800

    add translation schemes and functions

--- Chapter2/simple-translator.c
+++ Chapter2/simple-translator.c
@@ -5,17 +5,48 @@
 // include parser, lexer structures right here :>
 
 /*
-This translator follows a right-recursive grammar production rules:
+This translator translates infix notation to postfix notation
+It follows right-recursive grammar production rules:
 
 A -> term R
 B -> + term | - term | null_string
 
 Of course we need the translation schemes as well.
 
+expr -> term rest
+rest -> + term {print(`+`)} rest
+      | - term {print(`-`)} rest
+	  | null_string
+term -> 0 {print(`0`)}
+	  | 1 {print(`1`)}
+	  | 2 {print(`2`)}
+	  | 3 {print(`3`)}
+	  | 4 {print(`4`)}
+	  | 5 {print(`5`)}
+	  | 6 {print(`6`)}
+	  | 7 {print(`7`)}
+	  | 8 {print(`8`)}
+	  | 9 {print(`9`)}
 */
+
+
+// using the translation scheme, we can now make the program :>
+
+// match() checks the syntax errors. Expects a string input.
+void match(){
+
+}
+
+void term(){
+
+}
 
 void rest(){
 
+}
+
+void expr(){
+	term(); rest();
 }
 
 int main(){
