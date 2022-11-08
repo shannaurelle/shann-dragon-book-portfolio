45fe444bc675d0590278dfb708aeb5ebb0990ea9
shannaurelle <shannaurelleg@gmail.com>
2022-11-08 21:53:13 +0800

    new initial commit for simple translation

---
+++ Chapter2/simple-translator.c
@@ -0,0 +1,26 @@
+// Chapter 2: A syntax-directed translator in C
+
+#include <stdio.h>
+
+// include parser, lexer structures right here :>
+
+/*
+This translator follows a right-recursive grammar production rules:
+
+A -> term R
+B -> + term | - term | null_string
+
+Of course we need the translation schemes as well.
+
+*/
+
+void rest(){
+
+}
+
+int main(){
+
+// include translator structure right here :>
+
+return 0;
+}
