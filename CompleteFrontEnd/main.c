736179c5b4e4d11c689c0cae87bebe6bedc9235d
shannaurelle <shannaurelleg@gmail.com>
2022-12-02 11:38:50 +0800

    simple main program driver template

---
+++ CompleteFrontEnd/main.c
@@ -0,0 +1,9 @@
+// main program for compiler front-end
+#include "stdio.h"
+
+int main(){
+	// include lexer initialization here
+	// include parser initialization here
+	// run parser
+	printf("\n");
+}
